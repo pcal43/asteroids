344cd18977bcc9492dc2e037d4945fd45fb9cc77
Patrick Calahan <github@pcal.net>
2026-04-06 16:21:36 -0700

    checkpoint

--- pico8-home/carts/mycart/src/Controller.lua
+++ pico8-home/carts/mycart/src/Controller.lua
@@ -2,13 +2,26 @@
 -- top-level controller, manages overall flow of the game between screens
 
 local currentScreen = nil
+local flow = nil
 
 function _init()
-	currentScreen = GameScreen.new()
+	flow = cocreate(function()
+		while true do
+			currentScreen = TitleScreen.new()
+			while not currentScreen.isDone do
+				yield()
+			end
+			currentScreen = GameScreen.new()
+			while not currentScreen.isDone do
+				yield()
+			end
+		end
+	end)	
 end
 
 function _update()
 	if (currentScreen) currentScreen:update()
+	assert(coresume(flow))
 end
     
 function _draw()
